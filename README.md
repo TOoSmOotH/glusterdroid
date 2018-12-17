44a9f2e77888186bc00f55d0835f1045aacedd6a
Mike Reeves <luke@geekempire.com>
2018-12-16 19:41:26 -0500

    Testing this

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Semi Automated Setup of GlusterFS on odroid HC2
 This is probably not the most optimal of things. There are much better ways to
 do things but this project was never meant to show off any 1337 bash skillz. It was just
 an easy way for me to consistently build new gluster nodes and have them work. There
-are no guarantees and this is use at your won risk.
+are no guarantees and this is use at your own risk.
 
 
 - This has been tested on Odroid HC2 but would probably work on the 2.5" version.
@@ -16,7 +16,7 @@ That part is up to you.
 
 ## Installation:
 
-I am using Ubuntu 18.04 on my odroid HC2s. You can download it from the Odroid forum.
+I am using [Ubuntu 18.04](https://forum.odroid.com/viewtopic.php?t=27449) on my odroid HC2s. You can download it from the Odroid forum.
 First things first, let's install git. You can run an update here if you like as well but I call that in the script.
 It will reboot and you will have to run the setup a second time. You will notices the script knows
 when you reboot and takes you to the encryption part.
@@ -36,13 +36,20 @@ when you reboot and takes you to the encryption part.
 **GTYPE** = Gluster Node Type. This only really matters if you plan on burst mining on free space.  
 **TELEGRAF** = 1 enables telegraf to be installed and 0 disables it. See Details below.  
 **INFLUXSERVER** = This is the URL for your influxDB server. This only matters is you enable Telegraf..  
-**BURST** = 1 enabled 0 disabled. See details below  
+**BURST** = 1 enabled 0 disabled. See details below
+**BURSTPOOL** = This is the pool you are mining on. I recommend using the default. Make sure you read up on joining a pool.
+you have to set the reward assignment to the pools address etc.
+**BURSTACCOUNT** = Set this to your account ID of the wallet you using to mine with.
+**BURSTDEADLINE** = This is where you set the deadline. Research this... It's based on how much space you have etc.
+**BURSTPATH** = This is the path. Default should work.   
 
 ## Telegraf
 Telegraf is a slick way to monitor what's going on with your setup. I will include a grafana dashboard that you will have to edit for your environment. I have included a custom monitoring script for disk temps that gets dropped into cron.
 
 ## Burst
-Might as well mine with your free space right? Enabling this feature allows you to copy plots to your drives and have your odroids mine. This document or this script don't cover whay you need to do to start burst mining.
+Might as well mine with your free space right? Enabling this feature allows you to copy plots to your drives and have your odroids mine. This document or this script don't cover what you need to do to start burst mining. I have only tested this in distributed mode. I am not sure what would happen in dispersed when you fill the drive up
+outside the brick. Its on the list of thins to try. I find that 50GB plot sizes seem to work the best for balancing out across the cluster. Your experience may be different though. I will be releasing a script that will track the size of your
+plots and the size of your data and delete older plot files to free up space for real data. Stay Tuned!
 
 
 Feel free to submit PRs to make this better.
--- odroid-setup.sh
+++ odroid-setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+################################
+###                          ###
+###      Config Section      ###
+###                          ###
+################################
+
 # Change stuff to match in here. Add ALL your gluster nodes to the clusterhosts before you run this script
 # Hostname of the node you are adding
 HN=gluster10tb06
@@ -20,12 +26,24 @@ TELEGRAF=1
 INFLUXSERVER=http://192.168.2.4:8086
 # Do you want to mine burst on your free space? Set to 0 to disable.
 BURST=1
+# Enter the Pool you want to use.
+BURSTPOOL=http://voiplanparty.com:8124
+# Don't know if you really need this but this is the account id for which you are mining.
+BURSTACCOUNT=2163918534933052101
+# This is based on total TB you are mining with. Do research on the pool and the space you have for mining.
+BURSTDEADLINE=11087847
 # This path is dependant on the type of volume in gluster you are creating
 if [ $GTYPE == "distributed" ]; then
   BURSTPATH=/$MOUNTPOINT/$BRICKNAME/burst
 else
   BUSTPATH=/$MOUNTPOINT/burst
 fi
+
+###############################
+###                         ###
+###    This is the magic    ###
+###                         ###
+###############################
 
 # Don't mess with anything below this unless you know what you are doing.
 if [ ! -f 1stboot ]; then
@@ -123,7 +141,6 @@ if [ -f 1stboot ]; then
   mkfs -t $FILESYSTEM -m 1 /dev/mapper/$CRYPTVOL
   echo " Closing the volume so we can apply the key to it for booting"
   cryptsetup -v luksClose $CRYPTVOL
-  sleep 20
   echo " Applying key to the volume. You will need that password again. Last time I promise."
   cryptsetup luksAddKey /dev/sda1 /root/keyfile
   echo "Setting up the crypttab"
@@ -132,7 +149,7 @@ if [ -f 1stboot ]; then
   sed -i "/CRYPTODISKS_MOUNT*/c\CRYPTDISKS_MOUNT=\"/root/keyfile\"" /etc/default/cryptdisks
   mkdir -p $MOUNTPOINT/$BRICKNAME
   echo "Adding the mount to /etc/fstab"
-  echo "/dev/mapper/$CRYPTVOL $MOUNTPOINT $FILESYSTEM default,rw  0 2" >> /erc/fstab
+  echo "/dev/mapper/$CRYPTVOL $MOUNTPOINT $FILESYSTEM default,rw  0 2" >> /etc/fstab
   echo "Mounting the encrypted drive"
   mount -a
 
@@ -146,17 +163,29 @@ if [ -f 1stboot ]; then
     # Add the job to Cron
     echo "*/10 * * * * root /usr/sbin/drivetemp.sh" > /etc/cron.d/hddtemp
     cp telegraf.conf /etc/telegraf/
+    sed -i "s/ACIDINFLUX/$INFLUXSERVER/g" /etc/hosts
     systemctl restart telegraf
     systemctl enable telegraf
   fi
   if [ $BURST == 1 ]; then
     echo "Installing Burst"
-    wget
+    wget https://github.com/TOoSmOotH/glusterdroid/releases/download/scav1.6.6/scavenger-1.6.6-odroid.tar.gz
     tar zxvf scavenger-1.6.6-odroid.tar.gz
     mkdir -p /opt
     mv release /opt/scavenger
+    cp burstconfig.yaml /opt/scavenger/config.yaml
+    sed -i "s/POOL/$BURSTPOOL/g" /opt/scavenger/config.yaml
+    sed -i "s/PLOTPATH/$BURSTPATH/g" /opt/scavenger/config.yaml
+    sed -i "s/DEADLINE/$BURSTDEADLINE/g" /opt/scavenger/config.yaml
+    sed -i "s/ACNT/$BURSTACCOUNT/g" /opt/scavenger/config.yaml
+
     # Install screen
     apt -y install screen
+    # Create a junky script
+    echo "cd /opt/scavenger" > /usr/sbin/scav.sh
+    echo "./scavenger" >> /usr/sbin/scav.sh
+    # Launch scavenger in the background. You can connect by doing screen -r scav
+    screen -dmS scav /usr/sbin/scav.sh
   fi
 
   # This should not be automated because there are a multitude of options here.
